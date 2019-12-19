8b6a128b13e24dab7ce9b9f65bc408e0afb48f63
OlgaZaglavnova <olga.zaglavnova@mail.ru>
2019-12-19 18:28:57 +0300

    JavaScript2 Lesson4 Task3. Укоротила код - вместо 4х функций сделала одну и 4 ее вызова с тремя параметрами.

--- js2 lesson4 task3/script3.js
+++ js2 lesson4 task3/script3.js
@@ -8,9 +8,6 @@ class UserData{
         this.textRegExp = /^.*$/gm;
     }
     init() {
-        /*document.querySelector('#name').addEventListener('submit', ev => {
-            ev.preventDefault();
-        });*/
         document.querySelector('.sendBtn').addEventListener('click', elem => {
             elem.preventDefault();
             this.checkAllFields()
@@ -19,59 +16,23 @@ class UserData{
     testStrings(inputStr, regexp) {
          return regexp.test(inputStr);
     }
-    testUserName(){
-        const username = document.querySelector('#name');
-        this.nameRegExp.lastIndex = 0;
-        if (this.testStrings(username.value, this.nameRegExp)){
-            username.classList.remove('redBorder');
-            document.querySelector('.nameError').classList.add('hidden');
+    testElem(selector, regexplocal, errorclass){
+        const elemObj = document.querySelector(selector);
+        regexplocal.lastIndex = 0;
+        if (this.testStrings(elemObj.value, regexplocal)){
+            elemObj.classList.remove('redBorder');
+            document.querySelector(errorclass).classList.add('hidden');
             return true;
         } else {
-            username.classList.add('redBorder');
-            document.querySelector('.nameError').classList.remove('hidden');
-        }
-    }
-    testPhoneNumber(){
-        const phonenumber = document.querySelector('#phone');
-        this.phoneRegExp.lastIndex = 0;
-        if (this.testStrings(phonenumber.value, this.phoneRegExp)){
-            phonenumber.classList.remove('redBorder');
-            document.querySelector('.phoneError').classList.add('hidden');
-            return true;
-        } else {
-            phonenumber.classList.add('redBorder');
-            document.querySelector('.phoneError').classList.remove('hidden');
-        }
-    }
-    testEmail(){
-        const email = document.querySelector('#email');
-        this.emailRegExp.lastIndex = 0;
-        if (this.testStrings(email.value, this.emailRegExp)){
-            email.classList.remove('redBorder');
-            document.querySelector('.emailError').classList.add('hidden');
-            return true;
-        } else {
-            email.classList.add('redBorder');
-            document.querySelector('.emailError').classList.remove('hidden');
-        }
-    }
-    testText(){
-        const txt = document.querySelector('#text');
-        this.textRegExp.lastIndex = 0;
-        if (this.testStrings(txt.value, this.textRegExp)){
-            txt.classList.remove('redBorder');
-            document.querySelector('.textError').classList.add('hidden');
-            return true;
-        } else {
-            txt.classList.add('redBorder');
-            document.querySelector('.textError').classList.remove('hidden');
+            elemObj.classList.add('redBorder');
+            document.querySelector(errorclass).classList.remove('hidden');
         }
     }
     checkAllFields(){
-        this.testUserName();
-        this.testPhoneNumber();
-        this.testEmail();
-        this.testText();
+        this.testElem('#name', this.nameRegExp, '.nameError');
+        this.testElem('#phone', this.phoneRegExp, '.phoneError');
+        this.testElem('#email', this.emailRegExp, '.emailError');
+        this.testElem('#text', this.textRegExp, '.textError');
     }
 }
 
