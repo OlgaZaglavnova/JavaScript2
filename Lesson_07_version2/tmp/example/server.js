58ad5dd7b4799ac4a1a06dd63f54374565ef2171
OlgaZaglavnova <olga.zaglavnova@mail.ru>
2020-01-01 16:15:02 +0300

    это лишнее было

--- Lesson_07_version2/tmp/example/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const staticServer = require('node-static');
-
-const file = new staticServer.Server('.');
-
-http.createServer((req, res) => {
-  file.serve(req, res)
-}).listen(3000);
